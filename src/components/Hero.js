1b5fc9eb84c009d2a2627d83b03e40792858a4e4
Adebisi Abiodun <abiodunadebisi614@gmail.com>
2022-03-07 06:39:53 +0100

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section>
+            <img />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activity led by 
+                one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+
+function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay="morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay="afternoon"
+  } else {
+    timeOfDay = "evening"
+  }
+  return (
+    <h1>Good {timeOfDay}</h1>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
